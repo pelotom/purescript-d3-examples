dd91f12fc5ed2ac1fc83cf9d38a70fac57e7193f
Tom Crockett <pelotom@gmail.com>
2014-06-29 19:51:59 -0700

    Add 'Let's Make A Bar Chart' example

---
+++ src/LetsMakeABarChart.purs
@@ -0,0 +1,46 @@
+module Graphics.D3.Examples.LetsMakeABarChart where
+
+import Debug.Trace
+
+import Graphics.D3.Base
+import Graphics.D3.Util
+import Graphics.D3.Selection
+import Graphics.D3.Scale
+
+foreign import showAny
+  "function showAny(d) { return d; }"
+  :: forall a. a -> String
+
+{- 
+  This is a PureScript adaptation of Mike Bostock's "Let's Make a Bar Chart" series
+  of tutorials for D3, which can be found here: http://bost.ocks.org/mike/bar/
+-}
+
+{-
+  Bar Chart 1 - adapted from http://bl.ocks.org/mbostock/7322386
+  
+  JavaScript version:
+  ===================
+  var data = [4, 8, 15, 16, 23, 42];
+
+  var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+  d3.select(".chart")
+    .selectAll("div")
+      .data(data)
+    .enter().append("div")
+      .style("width", function(d) { return x(d) + "px"; })
+      .text(function(d) { return d; });
+-}
+bar1 = do
+  let table = [4, 8, 15, 16, 23, 42]
+  x <- linearScale
+    |. domain 0 (max table)
+    |. range 0 420
+    |. freeze
+  select ".chart" do
+    selectAll "div" $ bind table $ enter $ append "div" do
+      attr "style" \d -> "width:" ++ show (x d) ++ "px"
+      text \d -> d
--- src/examples.js
+++ src/examples.js
@@ -1,2 +1,2 @@
 exports.rectangles = require("Graphics.D3.Examples.Rectangles");
-
+exports.bar = require("Graphics.D3.Examples.LetsMakeABarChart");
