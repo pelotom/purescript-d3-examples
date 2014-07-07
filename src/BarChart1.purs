e8e34808da3989113d4cffa1b299f7d3011fe099
Tom Crockett <pelotom@gmail.com>
2014-07-07 02:50:26 -0700

    Add original JS code snippets for comparison

--- src/BarChart1.purs
+++ src/BarChart1.purs
@@ -17,6 +17,26 @@ import Graphics.D3.SVG.Axis
 -- | This is a PureScript adaptation of part 1 of Mike Bostock's "Let's Make a Bar Chart" series:
 -- | http://bost.ocks.org/mike/bar/1/
 
+{-
+
+Original JavaScript code:
+=========================
+
+var data = [4, 8, 15, 16, 23, 42];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
+
+-}
+
 table = [4, 8, 15, 16, 23, 42]
 
 main = do
--- src/BarChart2.purs
+++ src/BarChart2.purs
@@ -17,6 +17,48 @@ import Graphics.D3.SVG.Axis
 -- | This is a PureScript adaptation of part 2 of Mike Bostock's "Let's Make a Bar Chart" series:
 -- | http://bost.ocks.org/mike/bar/2/
 
+{-
+
+Original JavaScript code:
+=========================
+
+var width = 420,
+    barHeight = 20;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var chart = d3.select(".chart")
+    .attr("width", width);
+
+d3.tsv("data.tsv", type, function(error, data) {
+  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  chart.attr("height", barHeight * data.length);
+
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.value); })
+      .attr("height", barHeight - 1);
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d.value) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.value; });
+});
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
+
+-}
+
 type NameAndValue = { name :: String, value :: Number }
 
 coerceNameAndValue :: forall a. a -> D3Eff NameAndValue
