7ca224f7d8bfb411b64d6c6af44208edc7d9a26d
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2015-07-13 23:05:27 +0100

    PS 0.7 changes

--- src/Examples.purs
+++
@@ -1,6 +0,0 @@
-module Graphics.D3.Examples where
-
-import qualified Graphics.D3.Examples.BarChart1 as BC1
-import qualified Graphics.D3.Examples.BarChart2 as BC2
-import qualified Graphics.D3.Examples.BarChart3 as BC3
-import qualified Graphics.D3.Examples.ForceLayout1 as FL1
